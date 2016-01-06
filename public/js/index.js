e4eb758da34faaaec4a2fb0a630da5b7ad107a54
Pedro Seac <pv.seac@gmail.com>
2016-01-05 22:11:45 -0200

    removing unecessary comments

--- public/js/index.js
+++ public/js/index.js
@@ -9,28 +9,4 @@ $(document).ready(function () {
 		$('header > #pseac').css('padding-top', parallax);
 		$('header > .opaque').css('background-color', 'rgba(0, 0, 0, ' + rgba + ')');
 	});
-
-	// var sections = $('section#info > section');
-	// $(document).scroll(function() {
-	// 	var pos = $(document).scrollTop();
-	// 	for (var i = 0 ; i < sections.length ; i++) {
-	// 		if (pos <= $(sections[i]).offset().top) {
-	// 			var name = $($(sections[i]).find('.name'));
-	// 			name.css('position', 'absolute');
-	// 			name.css('top', '-43px');
-	// 			name.css('left', '0');
-	// 		} else if (pos >= $(sections[i]).offset().top + $(sections[i]).innerHeight() + 10) {
-	// 			var name = $($(sections[i]).find('.name'));
-	// 			name.css('position', 'absolute');
-	// 			name.css('top', '-43px');
-	// 			name.css('left', '0');
-	// 		} else if (pos >= $(sections[i]).offset().top) {
-	// 			var name = $($(sections[i]).find('.name'));
-	// 			var left = name.offset().left;
-	// 			name.css('position', 'fixed');
-	// 			name.css('top', '0');
-	// 			name.css('left', left);
-	// 		}
-	// 	}
-	// });
 });
